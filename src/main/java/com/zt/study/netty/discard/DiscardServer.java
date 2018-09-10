341f21779545ddd48d4d637e7f5cbf9d0428a23b
zhangxiaohua <563375002@qq.com>
2018-09-10 17:14:11 +0800

    添加丢弃 和 输出方法

---
+++ src/main/java/com/zt/study/netty/discard/DiscardServer.java
@@ -0,0 +1,69 @@
+package com.zt.study.netty.discard;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @ Author: zxh
+ * @ Date: Created in 15:59 2018/9/10
+ * @ Description: 丢弃服务器，启动程序
+ * @ Modified By:
+ */
+public class DiscardServer {
+
+    private int port;
+
+    public DiscardServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            System.out.println("server start at 8888......");
+            //绑定并开始接受连接
+            ChannelFuture f = bootstrap.bind(port).sync();
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        }finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port;
+        if(args.length > 0){
+            port = Integer.parseInt(args[0]);
+        }else {
+            port = 8888;
+        }
+        DiscardServer discardServer = new DiscardServer(port);
+        discardServer.run();
+    }
+}
---
+++ src/main/java/com/zt/study/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,41 @@
+package com.zt.study.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @ Author: zxh
+ * @ Date: Created in 15:37 2018/9/10
+ * @ Description: 基于netty的丢弃处理器
+ * @ Modified By:
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //丢弃bytebuf中的内容
+//        ((ByteBuf)msg).release();
+        //输出收到的内容
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            //循环的方式接受消息
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+            //调用netty 提供的工具
+            System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+        } finally {
+            //释放收到的buf
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
